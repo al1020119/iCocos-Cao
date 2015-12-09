ffda7be3a18b297ae8bb853660af4bda6e5f8e31
iCocos <al10201119@163.com>
2015-12-09 12:45:12 +0800

    asda

---
+++ iCocos-Git/iCocos-Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iCocos-Git
+//
+//  Created by caolipeng on 15/12/9.
+//  Copyright © 2015年 caolipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iCocos-Git/iCocos-Git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iCocos-Git
+//
+//  Created by caolipeng on 15/12/9.
+//  Copyright © 2015年 caolipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
