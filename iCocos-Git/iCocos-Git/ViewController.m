450a2aebe772f52bd820c6653e720613e89c1d2c
iCocos <al10201119@163.com>
2015-12-09 13:22:27 +0800

    ios

--- iCocos-Git/iCocos-Git/ViewController.m
+++ iCocos-Git/iCocos-Git/ViewController.m
@@ -10,18 +10,4 @@
 
 @interface ViewController ()
 
-@end
-
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-@end
+@end 
